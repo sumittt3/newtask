e2c562d09a9bb913e9e72a7f0e96cfd5703b2118
sumittt3 <sumitthakur16000@gmail.com>
2024-12-21 19:21:12 +0530

    Update EmployeePage.jsx

--- src/Components/EmployeePage.jsx
+++ src/Components/EmployeePage.jsx
@@ -11,8 +11,8 @@ export default function EmployeePage(props) {
         const response = await axios.get(
           "https://hub.dummyapis.com/employee?noofRecords=10&idStarts=1001"
         );
-        if (response.data && response.data.length === 0) {
-          setError(true);
+         if (!response.data || response.data.length === 0) {
+          setError("No data found or API returned an empty response.");
           setLoading(false);
           return;
         }
